042676ca2172f21eca53da977648b548ae7b72af
tarabudampati <71600680+tarabudampati@users.noreply.github.com>
2021-04-08 20:16:19 -0500

    Update my_favorite.php
    adding myfavorite.php

--- my_favorite.php
+++ my_favorite.php
@@ -32,7 +32,74 @@
                require_once "header.php";
                require_once "nav_bar.php";
             ?>
-            MY FAVORITE PAGE
+            <h2 id="title">Favorite Dance</h2><br>
+<?php
+$cookie_name = "fav_dance";
+ $favoriteDanceName = "Kuchipudi";
+
+if(!isset($_COOKIE[$cookie_name])) {
+     echo "Your favorite dance is not set, go to preferences and enter your favorite dance.";
+     echo "Default is Kuchipudi.";
+     echo "Your favorite dance is not set. You can set your favorite dance in preferences.";
+     echo "Using the system default.";
+     $fav_status = "COOKIE_NOT_FOUND";
+     $favoriteDanceName = "Kuchipudi";
+} 
+/*else{
+    echo "$_COOKIE[$cookie_name]";
+    $result = $link->query("SELECT * FROM dances WHERE name =" .$_COOKIE[$cookie_name]);
+
+    echo $result;
+    if ( $result->num_rows == 0 ) {
+        echo "That dance doesn't exist. Please input another dance.";
+    }
+    else {
+        $favoriteDanceName = $_COOKIE[$cookie_name];
+        header('location: display_the_dance.php?name='.$favoriteDanceName);
+    }
+
+    $favoriteDanceName = $_COOKIE[$cookie_name];
+    $fav_status = "COOKIE_N_DANCE_ARE_FOUND";
+    $sql_query = "SELECT `name` FROM dances WHERE `name` = '$favoriteDanceName'";
+    $mysqli_result = $link->query($sql_query);
+
+    // If the dance doesn't exist, we will get EMPTY result_set
+    $num_rows = mysqli_num_rows($mysqli_result);
+    if ( $num_rows == 0) {
+        echo "Your favorite dance doesn't exist in the database";
+        echo "Using the system default.";
+        $fav_status = "DANCE_NOT_FOUND";
+        $favoriteDanceName = "Kuchipudi";
+    } 
+} */
+else{
+    $favoriteDanceName = $_COOKIE[$cookie_name];
+    $fav_status = "COOKIE_N_DANCE_ARE_FOUND";
+    $sql_query = "SELECT `id` FROM dances WHERE `name` = '$favoriteDanceName'";
+    echo "$sql_query";
+    $mysqli_result = $link->query($sql_query);
+ print_r($mysqli_result);
+    // If the dance doesn't exist, we will get EMPTY result_set
+    $num_rows = mysqli_num_rows($mysqli_result);
+    if ($num_rows>0) {
+        $row = mysqli_fetch_array($mysqli_result, MYSQLI_ASSOC);
+                
+            // Retrieve individual field value
+            $id = $row['id'];
+            echo $id;
+    }
+    if ( $num_rows == 0) {
+    
+        echo "Your favorite dance doesn't exist in the database";
+        echo "Using the system default.";
+        $fav_status = "DANCE_NOT_FOUND";
+        $favoriteDressName = "Kuchipudi";
+    } 
+}
+// By the time we come here, we will have either system default or correct preferrence from cookie
+header('location: read.php?id='.$id);
+
+?>
     </div>
 </body>
 </html>
